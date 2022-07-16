36349fa6f3df7751f13c31a34dc54ea8e06194ea
Nicolás Arévalo <inag.458@gmail.com>
2022-07-16 18:55:48 -0500

    Making it responsive

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { Favs } from './components/Favs'
 import { PerritosAleatorios } from './components/PerritosAleatorios'
 /* import { SubirPerrito } from './components/SubirPerrito' */
 /* API SHIT */
 /* ----------------------------------------- */
-const API_KEY = '?api_key=7207963f-ba39-4fb6-9297-91c30bfa1c42'
-const URL_FAV = 'https://api.thedogapi.com/v1/favourites'
-const URL_UPLOAD = 'https://api.thedogapi.com/v1/images/upload'
-const URL = 'https://api.thedogapi.com/v1/images/search?limit=2'
-const HEADERS = {
-  'Content-Type': 'application/json',
-}
 
 function App() {
   return (
